6a61da6fa72820fde09ae03a421e0c929b464fc5
Vaathi07 <167275186+Vaathi07@users.noreply.github.com>
2025-05-14 23:42:31 +0530

    Add files via upload

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,13 @@
+const accountId = 456010
+let accountEmail = "work.vaathi@gmail.com"
+var accountPassword = "12345"
+accountCity = "Ujjain"
+let accountState;
+
+accountEmail = "hello@.com"
+accountPassword = "21212121"
+accountCity = "Delhi"
+accountState = "MP"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
